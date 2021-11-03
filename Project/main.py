4e0282e6ac1c6a8373dc948bdbae0937a4f650ab
Mayank18-dotcom <mayank123chittora@gmail.com>
2021-11-03 18:48:53 +0530

    filter and clothes updated

---
+++ Project/main.py
@@ -0,0 +1,141 @@
+import flask
+from flask import Flask, render_template, Response,redirect,request, jsonify
+from numpy import dtype
+from camera import VideoCamera
+from flask_pymongo import PyMongo
+import os
+from beautify.Moisturizer import *
+from beautify.Facewash import *
+from beautify.Sunscreen import *
+import bson
+from bson.objectid import ObjectId
+app = Flask(__name__)
+
+CART=[]
+# 
+app.config["MONGO_URI"] = "mongodb+srv://admindb:admindatabase@cluster0-vlwic.mongodb.net/myntra"
+mongo = PyMongo(app)
+db_operations = mongo.db.products
+
+@app.route('/tryon/<file_path>',methods = ['POST', 'GET'])
+def tryon(file_path):
+	file_path = file_path.replace(',','/')
+	os.system('python tryOn.py ' + file_path)
+	return redirect('http://127.0.0.1:5000/',code=302, Response=None)
+
+@app.route('/tryall',methods = ['POST', 'GET'])
+def tryall():
+        CART = request.form['mydata'].replace(',', '/')
+        os.system('python test.py ' + CART)
+        render_template('checkout.html', message='')
+
+
+@app.route('/')
+def indexx():
+    return render_template('home.html')
+
+@app.route('/read')
+def read():
+    users = db_operations.find()
+    output = [{'Label' : user['Label'] } for user in users]
+    return jsonify(output)
+
+@app.route('/form', methods =["GET", "POST"])
+def formData():
+    if request.method == "POST":
+        filter = {}
+        filter['Label'] = request.form.get("label")
+        skin = request.form.getlist("skin")
+        for x in skin:
+            if x == "Combination":
+                filter['Combination'] = 1
+            if x == "Dry":
+                filter['Dry'] = 1
+            if x == "Normal":
+                filter['Normal'] = 1
+            if x == "Oily":
+                filter['Oily'] = 1
+            if x == "Sensitive":
+                filter['Sensitive'] = 1
+        price_min = int(request.form['price-min'])
+        price_max = int(request.form['price-max'])
+        filter['price'] = { "$lte" : price_max, "$gte" : price_min}
+        products = db_operations.find(filter)
+        output = [
+            {'Label' : product['Label'] ,
+            'Img' : product['img'],
+            'Brand' : product['brand'] ,
+            'Name':product['name'],
+            'Price':product['price'],
+            'Rating':product['rating'],
+            'Combination':product['Combination'],
+            'Dry':product['Dry'],
+            'Normal':product['Normal'],
+            'Oily':product['Oily'],
+            'Sensitive':product['Sensitive'],
+            'Ingredients':product['ingredients'],
+            '_id' : product['_id']
+            }
+            for product in products]
+        return render_template("formoutput.html",output = output)
+    return render_template('form.html')
+
+@app.route("/details/<dbid>")
+def insert_one(dbid):
+    product=db_operations.find_one({'_id':bson.ObjectId(oid=str(dbid))})
+    output = [
+            {'Label' : product['Label'] ,
+            'Img' : product['img'],
+            'Brand' : product['brand'] ,
+            'Name':product['name'],
+            'Price':product['price'],
+            'Rating':product['rating'],
+            'Combination':product['Combination'],
+            'Dry':product['Dry'],
+            'Normal':product['Normal'],
+            'Oily':product['Oily'],
+            '_id':product['_id'],
+            'Sensitive':product['Sensitive'],
+            'Ingredients':product['ingredients']}]
+    return render_template("details.html",output = output)
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/details/Face Moisturizer')
+def applyMoisturizer_fun():
+    applyMoisturizer()
+    return redirect("http://127.0.0.1:5000/form")
+
+@app.route('/details/Sunscreen')
+def applySunscreen_fun():
+    applySunscreen()
+    return redirect("http://127.0.0.1:5000/form")
+
+@app.route('/details/Face Wash And Cleanser')
+def applyFacewash_fun():
+    applyFacewash()
+    return redirect("http://127.0.0.1:5000/form")
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route("/cart/<file_path>",methods = ['POST', 'GET'])
+def cart(file_path):
+    global CART
+    file_path = file_path.replace(',','/')
+    print("ADDED", file_path)
+    CART.append(file_path)
+    return render_template("checkout.html")
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run()
--- README.md
+++ README.md
@@ -4,4 +4,91 @@ Repository for Myntra HackerRamp: Campus Edition 2021
 ## Phase 1 PPT
 https://docs.google.com/presentation/d/17DITVg5c5THc35s57EnLpCQPImE4iTpbUdG6fnfA45k/edit#slide=id.gf70e5ac737_0_77
 
-## Phase 2 begins now
+## Phase 2 
+
+# Virtual-Dressing-Room
+Virtual Dressing Room is an augmented reality dressing room where user or customer will allow to try apparel on their body it self.
+
+Virtual Dressing Room is an augmented reality dressing room where the image of the user is captured by the webcam or any other camera and given as an input. By using the face and body detection techniques, the video is scanned for the presence of human faces and then product images are masked. Now the masked image is superimposed and the user can see the images of the dresses and face accessories on them self.
+
+<h2>Project Setup Process :</h2>
+
+<h3>Step 1:</h3> 
+Please install any Python Environment or IDE such as Anaconda...<br>
+To run : 
+
+```
+\Project>python main.py
+```  
+
+('.' Run this command if you've already installed all the packages, otherwise follow from Step 2)
+
+ 
+<h3>Step 2: Packages to install :</h3> 
+
+```
+pip install cmake
+```
+
+<h3>Step 3:</h3> 
+
+```
+pip install dlib
+```
+If it doesn't work then, visit this Github repository and download.
+```
+https://github.com/datamagic2020/Install-dlib
+```
+
+then go to the downloads and copy the path where github repository is downloaded, and also copy the file name.
+```
+pip install "github repository path\filename"
+```
+Still not able to follow? Watch this to install dlib - ` https://www.youtube.com/watch?v=AUJKdehF2ZA `
+
+
+<h3>Step 4:</h3> 
+
+```
+pip install flask
+```
+
+<h3>Step 5:</h3> 
+
+```
+pip install imutils
+```
+
+<h3>Step 6:</h3> 
+
+```
+pip install opencv-python
+```
+
+<h3>Step 7:</h3> 
+
+```
+pip install tk
+```
+
+<h3>Step 8:</h3> 
+
+```
+pip install pillow
+```
+
+<h3>Step 9:</h3> 
+
+```
+pip install flask_pymongo
+```
+
+<h3>Step 10:</h3> 
+
+```
+pip install tk
+```
+
+<h4>Now RUN `python main.py` </h4>
+After successfully completing run process you will get one URL(local host url)...
+Click on that URL and you will redirect to Default Browser on your system...
